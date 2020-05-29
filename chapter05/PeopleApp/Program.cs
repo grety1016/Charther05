67e44619d241ffce838400006c47107e927b1a10
grety1016 <snl1016@163.com>
2020-05-29 17:40:09 +0800

    2

--- chapter05/PeopleApp/Program.cs
+++ chapter05/PeopleApp/Program.cs
@@ -8,25 +8,24 @@ namespace PeopleApp
     {
         static void Main(string[] args)
         { 
+            //声明变量，初始化变化，输出变量信息。
             var bob = new Person();
             bob.Name = "Bob smith";
             bob.DateOfBirth = new DateTime(1965,12,22);
             WriteLine(format: "{0} was born on {1: dddd, d MMMM yyyy}",arg0: bob. Name,arg1: bob.DateOfBirth);
-            bob.FavoriteAncientWonder =  WondersOfTheAncientWorld.StatueOfZeusAtOlympia; 
-
+            //新增bob变量的FavoriteAncientWonder的值。
+            bob.FavoriteAncientWonder =  WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+            //输出
             WriteLine(format:"{0}'s FavoriteAncientWonder is {1},It's integer is {2}.",arg0:bob.Name,arg1:bob.FavoriteAncientWonder,arg2:(int)WondersOfTheAncientWorld.StatueOfZeusAtOlympia);
-            
+           
+            //新增alice变量
+            var alice = new Person()
+            {
+                Name = "Alice Jones",
+                DateOfBirth = new DateTime(1984,7,3)
+            };
 
-            
-
-           var alice = new Person()
-           {
-               Name = "Alice Jones",
-               DateOfBirth = new DateTime(1984,7,3)
-
-           };
-
-           WriteLine(format:"{0} was born on {1:dd MMM yy}",arg0:alice.Name,arg1:alice.DateOfBirth);
+            WriteLine(format:"{0} was born on {1:dd MMM yy}",arg0:alice.Name,arg1:alice.DateOfBirth);
                       
         }
     }
