29df9ae406df487c7b8232bab261327e9fe739aa
grety1016 <snl1016@163.com>
2020-06-02 17:35:46 +0800

    1

--- chapter05/PacktLibrary/PersonAutoGen.cs
+++ chapter05/PacktLibrary/PersonAutoGen.cs
@@ -1,7 +1,25 @@
+using System;
+
 namespace Packt. Shared
 {
+    //Class Person 
     public partial class Person
     {
-        
+        //property Origin
+        public string Origin
+        {
+            get
+            {
+                return $"{Name} was born on {HomePlanet}";
+            }
+        }
+
+        //property Greting
+        public string Greeting() => $"{Name} says 'Hello!'";
+
+        //property Age
+        public int Age => DateTime.Today.Year - DateOfBirth.Year;
     }
+
+
 }
